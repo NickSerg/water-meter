71f454690364067f55ecc055adae6027e215b5fc
Nikolay Tur <tur.nikolaj@gmail.com>
2015-02-10 22:02:47 +0300

    add asp.net mvc solution

---
+++ WaterMeter/WM.AspNetMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WM.AspNetMvc.MvcApplication" Language="C#" %>
---
+++ WaterMeter/WM.AspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WM.AspNetMvc.Startup))]
+namespace WM.AspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
