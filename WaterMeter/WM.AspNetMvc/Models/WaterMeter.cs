c028190606559f38f1813f51da6088f44420f595
Nikolay Tur <tur.nikolaj@gmail.com>
2015-02-11 11:41:21 +0300

    implement model

---
+++ WaterMeter/WM.AspNetMvc/Models/WaterMeter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WM.AspNetMvc.Models
+{
+    public class WaterMeter
+    {
+        [Key]
+        [Required]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Холодная вода")]
+        public int Cold { get; set; }
+
+        [Required]
+        [Display(Name = "Горячая вода")]
+        public int Hot { get; set; }
+
+        [Required]
+        [Display(Name = "Период показаний")]
+        public DateTime Period { get; set; }
+
+        [Required]
+        [Display(Name = "Дата регистрации")]
+        public DateTime Created { get; set; }
+    }
+}
---
+++ WaterMeter/WM.AspNetMvc/Models/WaterMeterDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace WM.AspNetMvc.Models
+{
+    public class WaterMeterDataContext : DbContext
+    {
+        public WaterMeterDataContext()
+            : base("WaterMeterConnection")
+        {
+        }
+
+        public DbSet<WaterMeter> WaterMeters { get; set; }
+    }
+}
