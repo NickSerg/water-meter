22128448ffa8270ee0de28a2f837836ce5c976d0
Nikolay Tur <tur.nikolaj@gmail.com>
2015-02-16 17:29:50 +0300

    update ui contact

--- WaterMeter/WM.AspNetMvc/Controllers/HomeController.cs
+++ WaterMeter/WM.AspNetMvc/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using WM.AspNetMvc.Models;
 
 namespace WM.AspNetMvc.Controllers
 {
@@ -12,7 +16,16 @@ namespace WM.AspNetMvc.Controllers
 
         public ActionResult Contact()
         {
-            return View();
+            var appDatePath = Server.MapPath("~/App_Data");
+            var configFile = Path.Combine(appDatePath, "Config");
+            Config config = null;
+            if (System.IO.File.Exists(configFile))
+            {
+                var configJson = System.IO.File.ReadAllText(configFile, Encoding.GetEncoding(1251));
+                config = JsonConvert.DeserializeObject<Config>(configJson);
+            }
+
+            return View(config ?? Config.Default);
         }
     }
 }
---
+++ WaterMeter/WM.AspNetMvc/Models/Config.cs
@@ -0,0 +1,22 @@
+﻿namespace WM.AspNetMvc.Models
+{
+    public class Config
+    {
+        public Contact Contact { get; set; }
+
+        public static Config Default
+        {
+            get
+            {
+                return new Config {Contact = new Contact {Email = "support@watermeter.com"}};
+            }
+        }
+    }
+    public class Contact
+    {
+        public string Sity { get; set; }
+        public string Home { get; set; }
+
+        public string Email { get; set; }
+    }
+}
